07fbfab0ba8ebeeac5b036c3a5d7524ec0e423c8
Konstantin <glonas22808@mail.ru>
2020-11-04 19:29:01 +0300

    some film

--- app/Http/Controllers/TimingController.php
+++ app/Http/Controllers/TimingController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\FilmModel;
 
 class TimingController extends Controller
 {
-
+    public function __construct(FilmModel $film)
+    {
+        $this->$film = $film;
+    }
+    public function index()
+    {
+        $film = new FilmModel();
+        $filmList = $film::all();
+        $filmList = isset($filmList[0]) ? $filmList : null;
+        return view('timing', compact('filmList'));
+    }
 }
